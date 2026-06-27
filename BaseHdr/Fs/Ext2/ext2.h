d13d918537f4c3913c3892ce552eac86e1df60ad
grep-name <pranav0bisht@gmail.com>
2026-06-27 22:19:53 +0530

    little fix

--- BaseHdr/Fs/Ext2/ext2.h
+++ BaseHdr/Fs/Ext2/ext2.h
@@ -211,6 +211,6 @@ typedef struct _ext2_fs_ {
 /*
  * Ext2Initialise -- mount the file system
  */
-extern void Ext2Initialise(AuVDisk* vdisk, char* mountname);
+AuVFSNode Ext2Initialise(AuVDisk* vdisk, char* mountname);
 
 #endif
