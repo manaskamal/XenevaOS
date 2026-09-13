a117e8cb00b747dd37ae23544f9d104739fb1155
Gunit kumar <43469241+Fexaop@users.noreply.github.com>
2026-09-14 00:55:38 +0530

    feat: add udpecho localhost test

--- Scripts/Linux/build_and_run_qemu.sh
+++ Scripts/Linux/build_and_run_qemu.sh
@@ -399,7 +399,6 @@ QEMU_ARGS=(
     # 1af4:1001 the shorthand defaults to --axiss
     -drive file=fat.img,format=raw,if=none,id=blk0
     -device virtio-blk-pci,drive=blk0,disable-legacy=on
-    -netdev user,id=net0
     -netdev user,id=net0,ipv6=on,ipv6-net=fec0::/64,ipv6-host=fec0::2
     -device virtio-net-pci,netdev=net0
     -device ramfb
